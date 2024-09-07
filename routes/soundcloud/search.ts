86e3d7c4b093f73f9b2ad99d8b82d91a64214576
Sander Spilleman <sander@spilleman.nl>
2024-09-07 08:49:11 +0200

    2024-09-07T08-49-11

--- routes/soundcloud/search.ts
+++ routes/soundcloud/search.ts
@@ -1,19 +1,19 @@
 import { FreshContext, Handlers } from "$fresh/server.ts";
 import { search } from "$services/soundcloud.ts";
-import { kv } from "$connections/kv.ts";
+// import { kv } from "$connections/kv.ts";
 
 export const handler: Handlers = {
     async GET(_req: Request, _ctx: FreshContext): Promise<Response> {
-        const existing = await kv.get(["soundcloud", "search"]);
-        if (existing.value) {
-            console.log("existing search");
-            return new Response(JSON.stringify(existing.value), {
-                status: 200,
-                headers: { "Content-Type": "application/json" },
-            });
-        }
+        // const existing = await kv.get(["soundcloud", "search"]);
+        // if (existing.value) {
+        //     console.log("existing search");
+        //     return new Response(JSON.stringify(existing.value), {
+        //         status: 200,
+        //         headers: { "Content-Type": "application/json" },
+        //     });
+        // }
         const tracks = await search();
-        await kv.set(["soundcloud", "search"], tracks?.collection);
+        // await kv.set(["soundcloud", "search"], tracks?.collection);
         console.log("tracks", tracks?.collection.length);
         return new Response(JSON.stringify(tracks?.collection), {
             status: 200,
